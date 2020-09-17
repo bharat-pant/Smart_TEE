e62a5caed00529372ccdb36a40b68384b00517fb
bharat-pant <bpant268@gmail.com>
2020-09-17 19:38:50 +0530

    by-pass-updating of the loops, code refinement and timer addition

--- Bharat opencv skeleton.py
+++ Bharat opencv skeleton.py
@@ -1,28 +1,23 @@
-import cv2
-import multiprocessing
-import time
-cars_lane_1, cars_lane_2,cars_lane_3,cars_lane_4,set_lane_1,set_lane_2,set_lane_3,set_lane_4 = 0,0,0,0,0,0,0,0
+import cv2,multiprocessing,time
+cars_lane_1, cars_lane_2,cars_lane_3,cars_lane_4,set_lane_1,set_lane_2,set_lane_3,set_lane_4,flag1,flag2,flag3,flag4 = 0,0,0,0,0,0,0,0,0,0,0,0
 lane_order=[]
-flag1,flag2,flag3,flag4=0,0,0,0
-threshold=15
+threshold=16
 car_cascade = cv2.CascadeClassifier('cars121004.xml')           # Trained XML classifiers describes some features of some object we want to detect
 
 
-def lane_check_1(q1,fg1,value):
-    value.append("lane1")
-    global cars_lane_1
-    global set_lane_1
+def lane_check_1(q1,fg1):
+    global cars_lane_1,set_lane_1
     set_lane_1=set()
     cap = cv2.VideoCapture(0)
-
     t_end=time.time()+10
+    
     while time.time()<t_end:
         ret, frames = cap.read()                                    # reads frames from a video
         gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         cars_lane_1=len(cars)
         set_lane_1.add(cars_lane_1)
-        q1.put(max(set_lane_1))
+
 
     ######################################################################### last commit made a list to store max value
         for (x,y,w,h) in cars:
@@ -31,10 +26,11 @@ def lane_check_1(q1,fg1,value):
         cv2.imshow('video2', frames)                                # Display frames in a window
         if cv2.waitKey(33) == 27 or max(set_lane_1)>threshold:                              #threshold ki jagah 10 dala hai
             break
+    q1.put(max(set_lane_1))
     fg1.put(1)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usage
 
-def lane_check_2(q2,fg2,value):
+def lane_check_2(q2,fg2):
     global cars_lane_2
     global set_lane_2
     set_lane_2=set()
@@ -46,22 +42,17 @@ def lane_check_2(q2,fg2,value):
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         cars_lane_2=len(cars)
         set_lane_2.add(cars_lane_2)
-        q2.put(max(set_lane_2))
-        print(value)
     ###############################################################commit change 2
         for (x, y,w,h) in cars:
             cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
         cv2.imshow('video2', frames)                                # Display frames in a window
-        if cv2.waitKey(33) == 27 or max(set_lane_2)>threshold or value[0]=="hello":                                # Wait for Esc key to stop
-            # value[0] = 1
-            # value[1] = 1
-            # value[2] = 1
-            # value[3] = 1
+        if cv2.waitKey(33) == 27 or max(set_lane_2)>threshold:                                # Wait for Esc key to stop
             break
+    q2.put(max(set_lane_2))
     fg2.put(1)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usag
 
-def lane_check_3(q3,fg3,value):
+def lane_check_3(q3,fg3):
     global cars_lane_3
     global set_lane_3
     set_lane_3=set()
@@ -74,23 +65,19 @@ def lane_check_3(q3,fg3,value):
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         cars_lane_3=len(cars)
         set_lane_3.add(cars_lane_3)
-        q3.put(max(set_lane_3))
 
 
         for (x,y,w,h) in cars:
             cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
 
         cv2.imshow('video2', frames)                                # Display frames in a window
-        if cv2.waitKey(33) == 27 or max(set_lane_3)>threshold or value[0]=="hello":            #threshold ki jagah 10 dala hai                       # Wait for Esc key to stop
-            # value[0] = 1
-            # value[1] = 1
-            # value[2] = 1
-            # value[3] = 1
+        if cv2.waitKey(33) == 27 or max(set_lane_3)>threshold:            #threshold ki jagah 10 dala hai                       # Wait for Esc key to stop
             break
+    q3.put(max(set_lane_3))
     fg3.put(1)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usage
 
-def lane_check_4(q4,fg4,value):
+def lane_check_4(q4,fg4):
     global cars_lane_4
     global set_lane_4
     set_lane_4=set()
@@ -103,18 +90,14 @@ def lane_check_4(q4,fg4,value):
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         cars_lane_4=len(cars)
         set_lane_4.add(cars_lane_4)
-        q4.put(max(set_lane_4))
 
         for (x,y,w,h) in cars:
             cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
 
         cv2.imshow('video2', frames)                                # Display frames in a window
-        if cv2.waitKey(33) == 27 or max(set_lane_4)>threshold or value[0]=="hello":            #threshold ki jagah 10 dala hai                       # Wait for Esc key to stop
-            # value[0] = 1
-            # value[1] = 1
-            # value[2] = 1
-            # value[3] = 1
+        if cv2.waitKey(33) == 27 or max(set_lane_4)>threshold:      # Wait for Esc key to stop
             break
+    q4.put(max(set_lane_4))
     fg4.put(1)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usage
 
@@ -130,18 +113,12 @@ if __name__ == '__main__':
         fg2 = multiprocessing.Queue()
         fg3 = multiprocessing.Queue()
         fg4 = multiprocessing.Queue()
-        check = multiprocessing.Queue()
-
-        value.append(0)
-        value.append(0)
-        value.append(0)
-        value.append(0)
 
         while True:
-            process1=multiprocessing.Process(target=lane_check_1,args=(q1,fg1,value))
-            process2=multiprocessing.Process(target=lane_check_2,args=(q2,fg2,value))
-            process3=multiprocessing.Process(target=lane_check_3,args=(q3,fg3,value))
-            process4=multiprocessing.Process(target=lane_check_4,args=(q4,fg4,value))
+            process1=multiprocessing.Process(target=lane_check_1,args=(q1,fg1,))
+            process2=multiprocessing.Process(target=lane_check_2,args=(q2,fg2,))
+            process3=multiprocessing.Process(target=lane_check_3,args=(q3,fg3,))
+            process4=multiprocessing.Process(target=lane_check_4,args=(q4,fg4,))
             process1.start(),process2.start(), process3.start(), process4.start()
             flag1 = fg1.get()
             flag2 = fg2.get()
@@ -149,7 +126,6 @@ if __name__ == '__main__':
             flag4 = fg4.get()
 
             if flag1==1 or flag2==1 or flag3==1 or flag4==1:
-                print(value)
                 process1.terminate()
                 process2.terminate()
                 process3.terminate()
@@ -158,6 +134,7 @@ if __name__ == '__main__':
                 lane_order.append(q2.get())
                 lane_order.append(q3.get())
                 lane_order.append(q4.get())
+                print(lane_order)
 
 '''flag updation to be performed'''
 
