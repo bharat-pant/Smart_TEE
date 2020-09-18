98d55560d197a569fd3aaf42f6d8d1fd17d68c2b
Divya <divyajinjholiya99@gmail.com>
2020-09-18 22:12:38 +0530

    update

--- Divya opencv skeleton.py
+++ Divya opencv skeleton.py
@@ -1,126 +1,101 @@
-
 import cv2
 import time
 from multiprocessing import Process
+
 def func1():
-    global a1
-    cap = cv2.VideoCapture('C:\Users\dharmbir\Desktop\bdh-pro-2\Car.mp4')
-    car_cascade = cv2.CascadeClassifier('C:\Users\dharmbir\Desktop\bdh-pro-2\cars121004.xml')
+    global lane_1
+    lane_1 = list()
+    cap = cv2.VideoCapture(r'Car.mp4')
+    car_cascade = cv2.CascadeClassifier(r'cars121004.xml')
     for i in range(3):
         ret, frames = cap.read()
-        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY) 
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         time.sleep(5)
-        a1=len(cars)
-        print('a1 is ',a1)
-        for (x,y,w,h) in cars: 
-            cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
+        a1 = len(cars)
+        lane_1.append(a1)
+        print('a1 is ', a1)
+        for (x, y, w, h) in cars:
+            cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 0)
         cv2.imshow('video2', frames)
-        if cv2.waitKey(33) == 27: 
+        if cv2.waitKey(33) == 27:
             break
+
+
 def func2():
-    global a2
-    cap = cv2.VideoCapture('C:\Users\dharmbir\Desktop\bdh-pro-2\Car.mp4')
-    car_cascade = cv2.CascadeClassifier('C:\Users\dharmbir\Desktop\bdh-pro-2\cars121004.xml')
+    global lane_2
+    lane_2 = list()
+    cap = cv2.VideoCapture(r'Car.mp4')
+    car_cascade = cv2.CascadeClassifier(r'cars121004.xml')
     for i in range(3):
         ret, frames = cap.read()
-        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY) 
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         time.sleep(6)
-        a2=len(cars)
-        print('a2 is ',a2)
-        for (x,y,w,h) in cars: 
-            cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
+        a2 = len(cars)
+        lane_2.append(a2)
+        print('a2 is ', a2)
+        for (x, y, w, h) in cars:
+            cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cv2.imshow('video2', frames)
-        if cv2.waitKey(33) == 27: 
+        if cv2.waitKey(33) == 27:
             break
+
+
 def func3():
-    global a3
-    cap = cv2.VideoCapture('C:\Users\dharmbir\Desktop\bdh-pro-2\Car_3.mp4')
-    car_cascade = cv2.CascadeClassifier('C:\Users\dharmbir\Desktop\bdh-pro-2\cars121004.xml')
+    global lane_3
+    lane_3 = list()
+    cap = cv2.VideoCapture(r'1038686156-preview.mp4')
+    car_cascade = cv2.CascadeClassifier(r'cars121004.xml')
     for i in range(3):
         ret, frames = cap.read()
-        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY) 
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         time.sleep(7)
-        a3=len(cars)
-        print('a3 is ',a3)
-        for (x,y,w,h) in cars: 
-            cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
+        a3 = len(cars)
+        lane_3.append(a3)
+        print('a3 is ', a3)
+        for (x, y, w, h) in cars:
+            cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cv2.imshow('video2', frames)
-        if cv2.waitKey(33) == 27: 
+        if cv2.waitKey(33) == 27:
             break
+
+
 def func4():
-    global a4
-    cap = cv2.VideoCapture('C:\Users\dharmbir\Desktop\bdh-pro-2\Car_4.mp4')
-    car_cascade = cv2.CascadeClassifier('C:\Users\dharmbir\Desktop\bdh-pro-2\cars121004.xml')
+    global lane_4
+    lane_4 = list()
+    cap = cv2.VideoCapture(r'Car_4.mp4')
+    car_cascade = cv2.CascadeClassifier(r'cars121004.xml')
     for i in range(3):
         ret, frames = cap.read()
-        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY) 
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
         time.sleep(8)
-        a4=len(cars)
-        print('a4 is ',a4)
-        for (x,y,w,h) in cars: 
-            cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
+        a4 = len(cars)
+        lane_4.append(a4)
+        print('a4 is ', a4)
+        for (x, y, w, h) in cars:
+            cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cv2.imshow('video2', frames)
-        if cv2.waitKey(33) == 27: 
+        if cv2.waitKey(33) == 27:
             break
 
-    
-if __name__=='__main__':
-     p1 = Process(target = func1)
-     p1.start()
-     p2 = Process(target = func2)
-     p2.start()
-     p3 = Process(target = func3)
-     p3.start()
-     p4 = Process(target = func4)
-     p4.start()
-     p1.join()
-     p2.join()
-     p3.join()
-     p4.join()
-l=[a1,a2,a3,a4]
-print(l)
+if __name__ == '__main__':
 
-
-cv2.destroyAllWindows() 
-"""
-cap = cv2.VideoCapture(0)
-car_cascade = cv2.CascadeClassifier('cars121004.xml')
-cap.get(20000)
-while True:
-    ret, frames = cap.read()
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY) 
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
-    time.sleep(5)
-    a1=len(cars)
-    print(a1)
-    for (x,y,w,h) in cars: 
-        cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
-    cv2.imshow('video2', frames)
-    if cv2.waitKey(33) == 27: 
-        break
+    p1 = Process(target=func1)
+    p1.start()
+    p2 = Process(target=func2)
+    p2.start()
+    p3 = Process(target=func3)
+    p3.start()
+    p4 = Process(target=func4)
+    p4.start()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
 
 
 
-import cv2
-import time
-cap = cv2.VideoCapture('Car.mp4')
-car_cascade = cv2.CascadeClassifier('cars121004.xml')
-while True: 
-    ret, frames = cap.read() 
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)  
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
-    time.sleep(5)
-    a1=len(cars)
-    print(a1)
-    for (x,y,w,h) in cars: 
-        cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2) 
-    cv2.imshow('video2', frames)
-    
-    if cv2.waitKey(33) == 27: 
-        break
 cv2.destroyAllWindows()
-"""
