9ec16b37979ffea3a5f501c94f2b3dc183d2a92e
bharat-pant <bpant268@gmail.com>
2020-09-26 16:30:53 +0530

    skeleton of the timer function to be called added in the program

--- Bharat opencv skeleton.py
+++ Bharat opencv skeleton.py
@@ -1,7 +1,7 @@
 import cv2,multiprocessing,time,math
 cars_lane_1, cars_lane_2,cars_lane_3,cars_lane_4,set_lane_1,set_lane_2,set_lane_3,set_lane_4,flag1,flag2,flag3,flag4 = 0,0,0,0,0,0,0,0,0,0,0,0
 lane_order=[]
-threshold=10
+threshold=35
 car_cascade = cv2.CascadeClassifier('cars121004.xml')           # Trained XML classifiers describes some features of some object we want to detect
 initial_timer=40
 
@@ -12,7 +12,7 @@ def lane_check_1(q1,fg1,order):
     t_end=time.time()+10
     queue_value=0
 
-    while time.time()<t_end:
+    while True:
         ret, frames = cap.read()                                    # reads frames from a video
         gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
@@ -27,6 +27,9 @@ def lane_check_1(q1,fg1,order):
             queue_value=1
             order.put(1)
             break
+        elif time.time()>=t_end:
+            order.put(0)
+            break
     q1.put(max(set_lane_1))
     fg1.put(queue_value)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usage
@@ -38,7 +41,7 @@ def lane_check_2(q2,fg2,order):
     cap = cv2.VideoCapture('videos/jan28.avi')
     t_end=time.time()+10
     queue_value=0
-    while time.time()<t_end:
+    while True:
         ret, frames = cap.read()                                    # reads frames from a video
         gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
@@ -52,6 +55,9 @@ def lane_check_2(q2,fg2,order):
             queue_value=1
             order.put(2)
             break
+        elif time.time()>=t_end:
+            order.put(0)
+            break
     q2.put(max(set_lane_2))
     fg2.put(queue_value)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usag
@@ -63,7 +69,7 @@ def lane_check_3(q3,fg3,order):
     cap = cv2.VideoCapture('videos/march9.avi')
     t_end = time.time() + 10
     queue_value=0
-    while time.time() < t_end:
+    while True:
         ret, frames = cap.read()                                    # reads frames from a video
         gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
@@ -77,6 +83,9 @@ def lane_check_3(q3,fg3,order):
             queue_value=1
             order.put(3)
             break
+        elif time.time()>=t_end:
+            order.put(0)
+            break
     q3.put(max(set_lane_3))
     fg3.put(queue_value)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usage
@@ -88,7 +97,7 @@ def lane_check_4(q4,fg4,order):
     cap = cv2.VideoCapture('videos/april21.avi')
     t_end = time.time() + 10
     queue_value=0
-    while time.time() < t_end:
+    while True:
         ret, frames = cap.read()                                    # reads frames from a video
         gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
         cars = car_cascade.detectMultiScale(gray, 1.1, 1)
@@ -103,46 +112,44 @@ def lane_check_4(q4,fg4,order):
             queue_value=1
             order.put(4)
             break
+        elif time.time()>=t_end:
+            order.put(0)
+            break
     q4.put(max(set_lane_4))
     fg4.put(queue_value)
     cv2.destroyAllWindows()                                         # De-allocate any associated memory usage
 
 
-def light_lane_1():
-    initial_timer=40
-    initial_timer_1= time.time()+initial_timer
+def light_lane_1(initial_timer1=40):
+    initial_timer_1= time.time()+initial_timer1
     while time.time()<initial_timer_1:
         print("GREEN")
         time.sleep(1)
 
 
-def light_lane_2():
-    initial_timer = 15
-    initial_timer_2 = time.time() + initial_timer
+def light_lane_2(initial_timer2=40):
+    initial_timer_2 = time.time() + initial_timer2
     while time.time() < initial_timer_2:
         print("GREEN_1")
         time.sleep(2)
 
 
-def light_lane_3():
-    initial_timer = 40
-    initial_timer_3 = time.time() + initial_timer
+def light_lane_3(initial_timer3=40):
+    initial_timer_3 = time.time() + initial_timer3
     while time.time() < initial_timer_3:
         print("GREEN")
         time.sleep(1)
 
 
-def light_lane_4():
-    initial_timer = 10
-    initial_timer_4 = time.time() + initial_timer
+def light_lane_4(initial_timer4=40):
+    initial_timer_4 = time.time() + initial_timer4
     while time.time() < initial_timer_4:
         print("GREEN_2")
         time.sleep(1)
 
 
-def red_light():
-    initial_timer = 40
-    initial_timer_red = time.time() + initial_timer
+def red_light(initial_timerR):
+    initial_timer_red = time.time() + initial_timerR
     while time.time() < initial_timer_red:
         print("RED")
         time.sleep(1)
@@ -170,7 +177,7 @@ if __name__ == '__main__':
             process2=multiprocessing.Process(target=lane_check_2,args=(q2,fg2,order,))
             process3=multiprocessing.Process(target=lane_check_3,args=(q3,fg3,order,))
             process4=multiprocessing.Process(target=lane_check_4,args=(q4,fg4,order,))
-            process1.start(),process2.start(), process3.start(), process4.start()#,process5.start(),process6.start(), process7.start(), process8.start()
+            process1.start(),process2.start(), process3.start(), process4.start()
             flag1 = fg1.get()
             flag2 = fg2.get()
             flag3 = fg3.get()
@@ -179,20 +186,14 @@ if __name__ == '__main__':
             a1,a2,a3,a4=q1.get(),q2.get(),q3.get(),q4.get()
 
             print(flag1,flag2,flag3,flag4)
-
-            if flag1==1 or flag2==1 or flag3==1 or flag4==1:
-                # process5.terminate()
-                # process6.terminate()
-                # process7.terminate()
-                # process8.terminate()
-                process1.terminate()
-                process2.terminate()
-                process3.terminate()
-                process4.terminate()
-                lane_order.append(a1)
-                lane_order.append(a2)
-                lane_order.append(a3)
-                lane_order.append(a4)
+            process1.terminate()
+            process2.terminate()
+            process3.terminate()
+            process4.terminate()
+            lane_order.append(a1)
+            lane_order.append(a2)
+            lane_order.append(a3)
+            lane_order.append(a4)
 
             if a1==0:
                 a1=1
@@ -204,7 +205,6 @@ if __name__ == '__main__':
                 a4=1
             else:
                 pass
-
 
 ###########################################################################################################
 
@@ -221,29 +221,30 @@ if __name__ == '__main__':
 
 ###########################################################################################################
 
-
             print(update_1,update_2,update_3,update_4)
-            for i in iter(order.get,'STOP'):
-                if i == 1:
+            for i in range(order.qsize()):
+                z=order.get()
+                print("value is ",z)
+                if z == 1:
                     print("LANE 1 GIVEN GREEN")
                     print("OTHER LANES GIVEN RED")
-                elif i == 2:
+                elif z == 2:
                     print("LANE 2 GIVEN GREEN")
                     print("OTHER LANES GIVEN RED")
                     process6.start()
                     process6.join()
-                elif i == 3:
+                elif z == 3:
                     print("LANE 3 GIVEN GREEN")
                     print("OTHER LANES GIVEN RED")
-                elif i == 4:
+                elif z == 4:
                     print("LANE 4 GIVEN GREEN")
                     print("OTHER LANES GIVEN RED")
                     process8.start()
-                else:
-                    print("NORMAL TIMER")
                 time.sleep(2)
                 print(lane_order)
                 print(lane_order.index(max(lane_order)))
+
+###################################################################################################################
 
 '''flag updation to be performed'''
 
